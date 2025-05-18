99861cc3c5d7006ee82585547ebfd4e0ba0cf22e
pudovikoff <pudovikov123@mail.ru>
2025-05-18 22:11:36 +0300

    clear repo

--- docs/README.md
+++
@@ -1,9 +0,0 @@
-# How to create docs?
-```
-cd docs
-pip install sphinx
-pip install sphinx-rtd-theme
-sphinx-apidoc -o source ../fractal/
-make html
-```
-Then you can deploy `/build`
--- fractal/loaders/simulations/__init__.py
+++
@@ -1,8 +0,0 @@
-from fractal.loaders.simulations.constant_fundings import \
-    ConstantFundingsLoader
-from fractal.loaders.simulations.monte_carlo import MonteCarloHourPriceLoader
-
-__all__ = [
-    "ConstantFundingsLoader",
-    "MonteCarloHourPriceLoader"
-]
